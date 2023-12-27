186b95bdd13295f2f6e122ffcaf2e4ba276efb65
minggureee <112376752+minseo0102@users.noreply.github.com>
2023-12-28 00:05:32 +0900

    [Silver IV] Title: 듣보잡, Time: 48 ms, Memory: 7580 KB -BaekjoonHub

---
+++ 백준/Silver/1764. 듣보잡/듣보잡.cc
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include<set>
+#include<algorithm>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int n, m;
+	cin >> n >> m;
+
+	set<string> arr;
+	for (int i = 0; i < n; i++) {
+		string str;
+		cin >> str;
+		arr.insert(str);
+	}
+
+	vector<string> answer;
+
+	for (int i = 0; i < m; i++) {
+		string str;
+		cin >> str;
+		
+		auto it = arr.find(str);
+		if (it != arr.end()) answer.push_back(str);
+	}
+
+	cout << answer.size() << "\n";
+	
+	sort(answer.begin(), answer.end());
+
+	for (int i = 0; i < answer.size(); i++) {
+		cout << answer[i] << "\n";
+	}
+
+	return 0;
+}
