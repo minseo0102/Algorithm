318cb1fe501cad0d436948827cf15efb99e73b15
minggureee <112376752+minseo0102@users.noreply.github.com>
2023-12-28 18:27:25 +0900

    [D3] Title: [S/W 문제해결 기본] 1일차 - View, Time: 8 ms, Memory: 13,380 KB -BaekjoonHub

---
+++ SWEA/D3/1206. ［S／W 문제해결 기본］ 1일차 － View/［S／W 문제해결 기본］ 1일차 － View.cpp
@@ -0,0 +1,68 @@
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 기본 제공코드는 임의 수정해도 관계 없습니다. 단, 입출력 포맷 주의
+// 아래 표준 입출력 예제 필요시 참고하세요.
+// 표준 입력 예제
+// int a;
+// float b, c;
+// double d, e, f;
+// char g;
+// char var[256];
+// long long AB;
+// cin >> a;                            // int 변수 1개 입력받는 예제
+// cin >> b >> c;                       // float 변수 2개 입력받는 예제 
+// cin >> d >> e >> f;                  // double 변수 3개 입력받는 예제
+// cin >> g;                            // char 변수 1개 입력받는 예제
+// cin >> var;                          // 문자열 1개 입력받는 예제
+// cin >> AB;                           // long long 변수 1개 입력받는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 표준 출력 예제
+// int a = 0;                            
+// float b = 1.0, c = 2.0;               
+// double d = 3.0, e = 0.0; f = 1.0;
+// char g = 'b';
+// char var[256] = "ABCDEFG";
+// long long AB = 12345678901234567L;
+// cout << a;                           // int 변수 1개 출력하는 예제
+// cout << b << " " << c;               // float 변수 2개 출력하는 예제
+// cout << d << " " << e << " " << f;   // double 변수 3개 출력하는 예제
+// cout << g;                           // char 변수 1개 출력하는 예제
+// cout << var;                         // 문자열 1개 출력하는 예제
+// cout << AB;                          // long long 변수 1개 출력하는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	//freopen("input.txt", "r", stdin);
+	for (int i = 1; i <= 10; i++) {
+		int n;
+		cin >> n;
+		vector<int> arr;
+		for (int j = 0; j < n; j++) {
+			int temp;
+			cin >> temp;
+			arr.push_back(temp);
+		}
+
+		int count = 0;
+		for (int j = 2; j < n-2; j++) {
+			int cent = arr[j];
+			if (arr[j - 2] < cent && arr[j - 1] < cent && arr[j + 1] < cent && arr[j + 2] < cent) {
+				int m1 = max(arr[j - 2], arr[j - 1]);
+				int m2 = max(arr[j + 1], arr[j + 2]);
+				count += cent - (max(m1, m2));
+			}
+		}
+
+		cout << "#" << i << " " << count << "\n";
+	}
+    return 0;
+}
